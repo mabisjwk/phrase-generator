e39105827c356c4644add93b6b656a3adb99f232
Mabisjwk <ogabi6035.2@gmail.com>
2026-09-08 22:17:35 -0500

    feat: exibindo frase fixa + estilizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PhraseGenerator from './components/PhraseGenerator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col font-serif gap-4 bg-gradient-to-r from-teal-500 to-sky-500 h-screen p-10 text-neutral-800 ">
+      <div className="bg-white rounded-xl p-4 shadow-lg">
+        <h1 className="txt-lg">Gerador de Frases</h1>
+        <hr className="h-px my-3"/>
+        <PhraseGenerator />
+      </div>
     </div>
   );
 }
---
+++ src/components/PhraseGenerator.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Phrase from './Phrase';
+
+const PhraseGenerator = () => {
+    const [phrase, setPhrase] = useState({ content: "I am stupid.", author: "Charles Leclerc"});
+
+    return (
+        <div className="min-h-20 flex flex-col gap-4 items-start">
+            <Phrase text={phrase.content} author={phrase.author} />
+            <button className="bg-gradient-to-r from-teal-500 to-sky-500 p-2 rounded-lg shadow-md text-white"> Nova Frase</button>
+        </div>
+    );
+};
+
+export default PhraseGenerator;
