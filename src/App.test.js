f6ea00b854b6c659b9274e5904cb9cc2ebe4fcc0
Mabisjwk <ogabi6035.2@gmail.com>
2026-09-09 00:02:28 -0500

    fix: corrigindo o teste de renderização

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza o título do gerador de frases', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const title = screen.getByText(/gerador de frases/i);
+
+  expect(title).toBeInTheDocument();
+});
+
+test('renderiza o botão de nova frase', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button', {
+    name: /nova frase/i
+  });
+
+  expect(button).toBeInTheDocument();
 });
